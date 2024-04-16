b883e99c79df516e582db3ea2fb7b627c5cfafe1
kosch104 <konstantin-schwarze@outlook.de>
2024-04-16 12:17:04 +0200

    Initial commit

---
+++ Mod.cs
@@ -0,0 +1,26 @@
+﻿using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+
+namespace CustomAssetPack
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(CustomAssetPack)}.{nameof(Mod)}")
+            .SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+        }
+
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+        }
+    }
+}
